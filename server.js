bc79b23334bd8482770b199065ef795563f11290
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-08-10 11:04:02 +0100

    Env vars added

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const mood=process.env.MOOD
 async function main() {
   while(true) {
     console.log("Microservices rock!");
+    console.log(`I am ${mood}`)
     await sleep(5000);
   }
 }
